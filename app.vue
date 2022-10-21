abca430555ed26243cf78f3fbcfeb97299b2168f
jinyong.park <jiny3360@nine2021.com>
2022-10-21 15:22:22 +0900

    project : twitter - tailwindcss & eslint 설정

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div :class="{'dark': darkMode}">
+    <div class="min-h-full">
+      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+        <h1>아하</h1>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+const darkMode = ref(true);
+
+</script>
